d53bff215cb9882f4c004a6b6b1a01256731ecc8
luizdebem <luizdebem99@gmail.com>
2022-04-25 17:50:16 -0300

    first page example

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Welcome = () => {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <p>Laravel, InertiaJS & ReactJS.</p>
+    </>
+  );
+};
+
+export default Welcome;
