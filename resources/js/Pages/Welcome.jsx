f2032d53d610cd6a9b280dd8350bc68de8e50d53
luizdebem <luizdebem99@gmail.com>
2022-04-26 22:42:47 -0300

    Active links

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -10,19 +10,23 @@ const Welcome = (props) => {
 
       {!!players.length
         && <table className="border">
-          <tr className="border">
-            <th className="p-2">Name</th>
-            <th className="p-2">Number</th>
-          </tr>
-          {players.map((player) => {
-            return (
-              <tr className="border text-center">
-                <td className="p-2">{player.name}</td>
-                <td className="p-2">{player.number}</td>
-              </tr>
-            );
-          })
-          }
+          <thead>
+            <tr className="border">
+              <th className="p-2">Name</th>
+              <th className="p-2">Number</th>
+            </tr>
+          </thead>
+          <tbody>
+            {players.map((player, index) => {
+              return (
+                <tr className="border text-center" key={index}>
+                  <td className="p-2">{player.name}</td>
+                  <td className="p-2">{player.number}</td>
+                </tr>
+              );
+            })
+            }
+          </tbody>
         </table>
       }
     </>
--- resources/js/Shared/Nav.jsx
+++ resources/js/Shared/Nav.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from "@inertiajs/inertia-react";
 
 const Nav = () => {
+  const page = window.location.pathname;
   return (
     <>
       <div className="mb-6">
@@ -11,9 +12,9 @@ const Nav = () => {
 
       <nav className="mb-6">
         <ul className="flex flex-row justify-around items-center">
-          <li><Link href="/">Home</Link></li>
-          <li><Link href="/login">Login</Link></li>
-          <li><Link href="/settings">Settings</Link></li>
+          <li><Link className={ page === "/" ? "font-bold underline" : "" } href="/">Home</Link></li>
+          <li><Link className={ page === "/login" ? "font-bold underline" : "" } href="/login">Login</Link></li>
+          <li><Link className={ page === "/settings" ? "font-bold underline" : "" } href="/settings">Settings</Link></li>
           <li><Link href="/logout" method="post" as="button">Logout</Link></li>
         </ul>
       </nav>
